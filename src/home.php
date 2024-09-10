4bb3a2e4851b91b8e0c2a932e692dd9bb87f187d
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-10 14:34:46 +0800

    created a styles and linked to home, for header, content and footer part

--- src/home.php
+++ src/home.php
@@ -1,3 +1,21 @@
 <?php
 
-echo 'home page';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="src/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h1>Header</h1>
+        </div>
+        <div class="content">
+            <h2>Content</h2>
+        </div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </body>
+</html>
