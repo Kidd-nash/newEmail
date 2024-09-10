d901490de1ee77353af185de336097b9cb4ae89a
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-10 14:12:51 +0800

    file for the action contact form

---
+++ src/contact-submit.php
@@ -0,0 +1,34 @@
+<?php
+
+ob_start();
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include_once('./src/email.php');
+   
+    $emailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $emailTo = 'admin@email.api';
+    $subject = 'customer contact message';
+    $content = "
+    <html>
+
+    <head>
+    </head>
+    <body>
+        <p>
+            Message from customer: 
+        </p>
+        <p>
+            $message
+        </p>
+    </body>
+    </html>
+    ";
+
+    sendEmail($emailTo, $emailFrom, $subject, $content);
+
+
+}
+ob_end_clean();
+// header("Location: http://localhost:8080/newCars/home.php");
+// die();
