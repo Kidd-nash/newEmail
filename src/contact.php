6d25da656935572745b6dbe34003bcf294290180
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-10 14:12:03 +0800

    contact page form

---
+++ src/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact us</title>
+    </head>
+    <body>
+        <form method="POST" action="/contact-submit">
+            <label>
+                Email:
+            </label>
+            <input type="email" name="email">
+            <br>
+            <label>
+                Message:
+            </label>
+            <textarea name="message"></textarea>
+            <br>
+            <button >Submit</button>
+        </form>
+    </body>
+</html>
