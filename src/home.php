d25c970e2d602d23b0c1d3f2dd369145c6797489
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-10 15:59:30 +0800

    divided parts for initial look

--- src/home.php
+++ src/home.php
@@ -9,7 +9,17 @@
     </head>
     <body>
         <div class="header">
-            <h1>Header</h1>
+            <div class="header-logo">
+                <p>Logo</p>
+                <img />
+            </div>
+            <div class="header-text">
+                <h1>Header Text</h1>
+            </div>
+            <div class="header-signup">
+                <a href="http://email.api:8080/register" target="_self">Register</a>
+                <a href="http://email.api:8080/login" target="_self">Log In</a>
+            </div>
         </div>
         <div class="content">
             <h2>Content</h2>
