366aae7507a6c8d0d77c8d6cb10df0a6bd967672
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-10 13:49:07 +0800

    added home page, new page, not found page

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php 
-echo 'email';
+
+$uri = strtok($_SERVER["REQUEST_URI"], '?');
+
+// $fullUri  = $_SERVER['REQUEST_URI'];
+// $uriElements = explode('?', $fullUri);
+// $uri = $uriElements[0];
+
+
+
+//c
+//r
+//u
+//d
+
+
+switch ($uri) {
+    case '/home':
+        include_once('./src/home.php');
+        break;
+    case '/new':
+        include_once('./src/new.php');
+        break;
+    default:
+        include_once('./src/404.php');
+}
