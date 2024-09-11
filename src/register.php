ee77ffef6acab75011809019b85dc660cb5e8d34
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-11 13:22:43 +0800

    created a from to create a new user

--- src/register.php
+++ src/register.php
@@ -1,3 +1,20 @@
 <?php
-
-echo 'register page';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <form method="POST" action="/registering">
+            <h2>Register</h2>
+            <label>Email:</label>
+            <input type="email" name="email" placeholder="Insert Email..."/><br>
+            <label>Username:</label>
+            <input type="text" name="user-name" placeholder="Create Username..."/><br>
+            <label>Password:</label>
+            <input type="text" name="password" placeholder="Create Password..."/><br>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
