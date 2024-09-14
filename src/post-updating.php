4ea614f6dcf00d6202384deb6c1c7afe742247b0
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-14 16:53:30 +0800

    working updating post

---
+++ src/post-updating.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+ob_start();
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include_once('./src/connection.php');
+
+    $id = $_POST["id"] ?? null;
+    $content = $_POST["content"] ?? null;
+    
+    $userQuery = $db->prepare("UPDATE post_a_note SET content = :content WHERE id = :id");
+    
+    $userQuery->execute(['content' => $content, 'id' => $id]);
+
+    $_SESSION['saved'] = true;
+
+}
+ob_end_clean();
+header("Location: http://email.api:8080/home");
+die();
