e5ade9786700a6844a8db58ea7217edc37fb5b25
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-14 20:38:13 +0800

    fixed and added classes for styling

--- src/home.php
+++ src/home.php
@@ -30,12 +30,15 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <title>Home</title>
     <link rel="stylesheet" href="src/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <div class="header">
+    <div class="header body-div">
         <div class="header-logo">
-            <p>Logo</p>
+            <p class="p-logo">Logo</p>
             <img />
         </div>
         <div class="header-text">
@@ -52,42 +55,44 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
             <?php endif; ?>
         </div>
     </div>
-    <div class="content">
-        <?php if (isset($_SESSION['accessDeniedError'])): ?>
-            <p>You can only post if you are logged in. <a href="/login">Log in here</a> </p>
-            <?php unset($_SESSION['accessDeniedError']) ?>
-        <?php endif; ?>
-
-        <?php if (!$isLoggedIn): ?>
-            <p>You can post stuff in here, but you have to be logged in first</p>
-        <?php else: ?>
-            <p>Post something?</p>
-            <form class="post-content" method="POST" action="/posting">
-                <label>Enter Post...</label>
-                <br>
-                <textarea name="content" rows="4" cols="50"></textarea>
-                <br>
-                <button type="submit">POST!</button>
-            </form>
-            <?php if (isset($isSaved)): ?>
-                <p>Saved succesfully</p>
+    <div class="content body-div">
+        <div class="content-posts">
+            <?php if (isset($_SESSION['accessDeniedError'])): ?>
+                <p>You can only post if you are logged in. <a href="/login">Log in here</a> </p>
+                <?php unset($_SESSION['accessDeniedError']) ?>
             <?php endif; ?>
-            <div class="posts">
-                <?php
-                foreach ($posts as $a_post) {
-                    echo '<div class="each_post">';
-                    echo '<p>' . $_SESSION['username'] . '</p>';
-                    echo '<p>' . $a_post['content'] . '</p>';
-                    echo '<p>' . $a_post['date_posted'] . '</p>';
-                    echo "<p><a href='http://email.api:8080/post-editing?editingId=" . $a_post["id"] . "'>Edit</a><p>";
-                    echo "<p><a href='http://email.api:8080/post-delete?deleteId=" . $a_post["id"] . "'>Delete</a><p>";
-                    echo '</div>';
-                }
-                ?>
-            </div>
-        <?php endif; ?>
+
+            <?php if (!$isLoggedIn): ?>
+                <p>You can post stuff in here, but you have to be logged in first</p>
+            <?php else: ?>                
+                <form class="post-content" method="POST" action="/posting">
+                    <p>Post something?</p>
+                    <label>Enter Post...</label>
+                    <br>
+                    <textarea name="content" rows="4" cols="50"></textarea>
+                    <br>
+                    <button type="submit">POST!</button>
+                </form>
+                <?php if (isset($isSaved)): ?>
+                    <p>Saved succesfully</p>
+                <?php endif; ?>
+                <div class="posts">
+                    <?php
+                    foreach ($posts as $a_post) {
+                        echo '<div class="each_post">';
+                        echo '<p>' . $_SESSION['username'] . '</p>';
+                        echo '<p>' . $a_post['content'] . '</p>';
+                        echo '<p>' . $a_post['date_posted'] . '</p>';
+                        echo "<p><a href='http://email.api:8080/post-editing?editingId=" . $a_post["id"] . "'>Edit</a><p>";
+                        echo "<p><a href='http://email.api:8080/post-delete?deleteId=" . $a_post["id"] . "'>Delete</a><p>";
+                        echo '</div>';
+                    }
+                    ?>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
-    <div class="footer">
+    <div class="footer body-div">
         <p>footer</p>
     </div>
 </body>
