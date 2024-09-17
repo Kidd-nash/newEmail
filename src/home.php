369234a4facf6f9d154fe21453b44619b891bfc0
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-17 17:00:02 +0800

    added id for elements that needs to turn dark mode, and changes class on each posts div

--- src/home.php
+++ src/home.php
@@ -30,21 +30,22 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <title>Home</title>
     <link rel="stylesheet" href="src/style.css">
+    <script type="text/javascript" src="src/dark-mode.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <div class="header body-div">
-        <div class="header-logo">
+    <div class="header body-div" id="header-div">
+        <div class="header-logo" id="header-logo">
             <p class="p-logo">Logo</p>
             <img />
         </div>
-        <div class="header-text">
+        <div class="header-text" id="header-text">
             <h1>Email Posts</h1>
         </div>
-        <div class="header-signup">
+        <div class="header-signup" id="header-signup">
             <?php if (!$isLoggedIn): ?>
                 <a class="signup" href="/register" target="_self">Register</a>
                 <a class="signup" href="/login" target="_self">Log In</a>
@@ -52,10 +53,11 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
                 <?= $_SESSION['username'] ?>
                 <p>, hey there.</p>
                 <a class="signout" href="/log-out">Log out</a>
+                <button onclick="darkMode()">&#xF46B;</button>
             <?php endif; ?>
         </div>
     </div>
-    <div class="content body-div">
+    <div class="content body-div" id="content-div">
         <div class="content-posts">
             <?php if (isset($_SESSION['accessDeniedError'])): ?>
                 <p>You can only post if you are logged in. <a href="/login">Log in here</a> </p>
@@ -65,7 +67,7 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
             <?php if (!$isLoggedIn): ?>
                 <p>You can post stuff in here, but you have to be logged in first</p>
             <?php else: ?>                
-                <form class="post-content" method="POST" action="/posting">
+                <form class="post-content" method="POST" action="/posting" id="content-form">
                     <p>Post something?</p>
                     <label>Enter Post...</label>
                     <br>
@@ -76,10 +78,10 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
                 <?php if (isset($isSaved)): ?>
                     <p>Saved succesfully</p>
                 <?php endif; ?>
-                <div class="posts">
+                <div class="posts" id="content-posts">
                     <?php
                     foreach ($posts as $a_post) {
-                        echo '<div class="each_post">';
+                        echo '<div class="each-post">';
                         echo '<p>' . $_SESSION['username'] . '</p>';
                         echo '<p>' . $a_post['content'] . '</p>';
                         echo '<p>' . $a_post['date_posted'] . '</p>';
@@ -92,7 +94,7 @@ $posts = $postQuery->fetchAll(PDO::FETCH_ASSOC);
             <?php endif; ?>
         </div>
     </div>
-    <div class="footer body-div">
+    <div class="footer body-div" id="footer-div">
         <p>footer</p>
     </div>
 </body>
