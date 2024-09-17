c19dec97d91fd0b015286277ecd1602faeef00a7
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-17 17:03:07 +0800

    echoed a message for the new idea of compiling functions

---
+++ src/message-list.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <ul>
+            <?php foreach($messageList as $msg): ?>
+                <li><?php echo $msg ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
