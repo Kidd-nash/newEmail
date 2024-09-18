4c736c9cbfdebddb1065ce920c99d0b870e43aa4
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-18 16:51:48 +0800

    added a function for the log in form and logging in method

--- src/Signup.php
+++ src/Signup.php
@@ -105,5 +105,49 @@ class Signup {
         sendEmail($emailTo, $emailFrom, $subject, $content);
 
     }
+
+    public function loginForm() {
+
+        ob_start();
+
+        include_once('./src/login-class.php');
+
+        return ob_get_clean();
+    }
+
+    public function submitLogin() {
+
+        session_start();
+        ob_start();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+            $user_name_or_email = $_POST["userNameOrEmail"];
+            $password = $_POST["password"];
+            $hash_password = md5($password);
+        
+        
+            $userQuery = $this->connection->prepare('SELECT * FROM email_users WHERE (username = :user_name_or_email OR email = :user_name_or_email) AND password = :hash_password');
+        
+            $userQuery->execute([
+                'user_name_or_email' => $user_name_or_email,
+                'hash_password' => $hash_password
+            ]);
+        
+            $user = $userQuery->fetch(PDO::FETCH_ASSOC);
+        
+            $userId = $user['id'] ?? null;
+        
+            if (!is_null($userId)) {
+                $_SESSION['userId'] = $userId;
+                $_SESSION['username'] = $user['username'];
+                ob_end_clean();
+                header("Location: http://email.api:8080/new-home");
+                die();
+            }
+        }
+        die('test');
+
+        return ob_get_clean();
+    }
     
 }
