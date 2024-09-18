2d2c01e0d3f3482c5b59f9c64d848e5204becb39
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-18 15:19:55 +0800

    added a template for after registering

---
+++ src/registered-class.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            a:link, a:visited {
+              background-color: rgb(115, 194, 251);
+              color: white;
+              padding: 14px 25px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              border-radius: 5;
+            }
+            
+            a:hover, a:active {
+              background-color: rgb(0, 128, 255);
+            }
+        </style>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo "Your username is: " . $user_name;
+            ?>
+        </p>
+        <p>
+            <?php
+                echo "Your password is: " . $password;
+            ?>
+        </p>
+        <a href="http://email.api:8080/home" target="_blank">Go back home to log in....</a>
+    </body>
+</html>
