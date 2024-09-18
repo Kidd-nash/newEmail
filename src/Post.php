627de189b6b71952f6a4147d1c6ab686cfc8d547
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-18 17:41:47 +0800

    adding a function for posting/creating post

--- src/Post.php
+++ src/Post.php
@@ -27,7 +27,37 @@ class Post {
 
     public function createPost() 
     {
-        return "creating";
+        session_start();
+
+        ob_start();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            session_start();
+
+            $isLoggedIn = false;
+            if (isset($_SESSION['userId'])) {
+                $isLoggedIn = true;
+            } else {
+                $_SESSION['accessDeniedError'];
+                ob_end_clean();
+                header("Location: http://email.api:8080/new-home");
+                die();
+            }
+
+            $id_plus = $_SESSION['userId'];
+
+            $content = $_POST['content'];
+            $author_id = $id_plus;
+
+            $userQuery = $this->connection->prepare('INSERT INTO post_a_note (content, date_posted, author_id) VALUES (:content, :date_posted, :author_id)');
+            $userQuery->execute(['content' => $content, 'date_posted' => date('Y-m-d'), 'author_id' => $author_id]);
+
+            $_SESSION['saved'] = true;
+        }
+        ob_end_clean();
+
+        header("Location: http://email.api:8080/new-home");
+        die();
     }
 
     public function listPost()
