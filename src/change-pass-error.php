4fa5365272f877dd50ed9ad09f2411aefcfc9cb2
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-23 09:59:05 +0800

    created a temp for error directory

---
+++ src/change-pass-error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Error</title>
+        <link rel="stylesheet" href="src/style.css">
+    </head>
+    <body>
+        <p>
+            Link to change passwrord has expired.<br>
+        </p>
+        
+        <a href="http://email.api:8080/forgot-password" target="_self" >Back to forgot password</a>
+    </body>
+</html>
