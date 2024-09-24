7c9d20721fe5d88ef6c960ed38455e94071797f9
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-24 16:35:03 +0800

    created a template for all posts each post that has comments

---
+++ src/all-post-tpl.php
@@ -0,0 +1,25 @@
+<div class="each-post">
+    <p><?php echo $_SESSION['username'] ?></p>
+    <p><?php echo $a_post['content'] ?></p>
+    <p><?php echo $a_post['date_posted'] ?></p>
+    <p><a href='http://email.api:8080/class-post-editing?editingId="<?php echo $a_post["id"] ?>"'>Edit</a></p>
+    <p><a href='http://email.api:8080/class-post-delete?deleteId="<?php $a_post["id"] ?>"'>Delete</a></p>
+    <?php foreach ($a_post['comments'] as $comment): ?>
+        <div class="each-comment">
+            <p><?php echo $comment['content'] ?> | by: <?php echo $comment['author_id'] ?></p>
+            <p>on: <?php echo $comment['date_posted'] ?></p>
+        </div>
+    <?php endforeach; ?>
+    <form method="POST" action="/comment-post" class="sign-up-form" id="sign-up-form">
+        <h2>Make a comment</h2>
+        <div class="form-div">
+            <p class="message-error" id="comment-error"></p>
+            <label>Comment:</label>
+            <textarea name="comment"></textarea>
+        </div>
+        <div class="form-div">
+            <input type="hidden" name="post-id" value="<?php echo $a_post["id"] ?>" />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</div>
